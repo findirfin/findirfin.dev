67ce457290d6ba8dee6c90e1c5da70955317c9f8
Findirfin Noldor <89416531+Findirfin@users.noreply.github.com>
2023-07-15 20:37:23 -0600

    Added hello world typing hover animation

--- script.js
+++ script.js
@@ -39,5 +39,40 @@ $(document).ready(function() {
         // Display the user info in the textarea element
         $('#user-info-display').val(userInfo);
       });
+
+
+
+      const letters = "AaBbCcDdEeFfGHhIiJKkLlMmNnOoPpQRrSsTtUuVvWwXxYyZz ";
+      //const letters = "01";
+
+
+      let interval = null;
+      
+      document.querySelector("h1").onmouseover = event => {  
+        let iteration = 0;
+        
+        clearInterval(interval);
+        
+        interval = setInterval(() => {
+          event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+              if(index < iteration) {
+                return event.target.dataset.value[index];
+              }
+            
+              return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+          
+          if(iteration >= event.target.dataset.value.length){ 
+            clearInterval(interval);
+          }
+          
+          iteration += 1 / 3;
+        }, 30);
+      }
+
+
   });
   
